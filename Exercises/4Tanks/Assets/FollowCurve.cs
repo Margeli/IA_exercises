51de5d3a51a7067b07ab25b8842ff819256f6561
Margeli <niniopantalones@gmail.com>
2018-10-08 14:01:59 +0200

    Update Exercises/4Tanks/Assets/FollowCurve.cs

--- Exercises/4Tanks/Assets/FollowCurve.cs
+++ Exercises/4Tanks/Assets/FollowCurve.cs
@@ -6,9 +6,13 @@ using BansheeGz.BGSpline.Components;
 public class FollowCurve : MonoBehaviour
 {
     float currentRatio = 0.0f;
+    
+    public BGCcMath path;
+    public float step = 0.01f;
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start ()
     {
 		
 	}
@@ -18,5 +22,11 @@ public class FollowCurve : MonoBehaviour
     {
         // TODO 1: Have a GameObject that follows the curve's path
         // Increase the ratio [0 to 1] and set the GameObject position to the respecive point in the curve
+        if (currentRatio > 1.0f) {
+            currentRatio = 0;
+        }
+       transform.position = path.CalcPositionByDistanceRatio(currentRatio); 
+        currentRatio+=step;
+
 	}
 }
