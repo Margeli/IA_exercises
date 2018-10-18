b78efbf1182a348d19fc5ad9f0ccabcbbe4c0d23
Margeli <niniopantalones@gmail.com>
2018-10-18 14:34:14 +0200

    added animations

---
+++ Exercises/AI_Animations/Assets/Scripts/Move.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour {
+
+	public GameObject target;
+    NavMeshAgent navMeshAgent;
+
+
+   
+
+    // Methods for behaviours to set / add velocities
+void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update () 
+	{
+        if (navMeshAgent && target) {
+            navMeshAgent.destination = target.transform.position;
+
+        }
+    }
+}
