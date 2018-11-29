1199eb7f207151061aadddb1d64ac212574a3710
Margeli <niniopantalones@gmail.com>
2018-11-29 14:00:45 +0100

    GOAP

---
+++ Exercises/GOAP/Assets/AIAgent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GOAP;
+
+public class AIAgent : MonoBehaviour , IGoap{
+
+
+    public List<Condition> state { get; set; }
+    public List<Goal> availableGoals { get; set; }
+    public List<Action> availableActions { get; set; }
+
+
+    private void Awake()
+    {
+        state = new List<Condition>();
+        availableGoals = new List<Goal>();
+        availableActions = new List<Action>();
+
+        foreach (Goal g in availableGoals)
+            g.OnGoalInitialize(this);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Exercises/GOAP/Assets/GOPE_Goal_Something.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GOAP;
+
+public class GOPE_Goal_Something : Goal {
+
+    public GOPE_Goal_Something() { }
+
+    public override void OnGoalInitialize(IGoap igoap)
+    {
+        this.igoap = igoap;
+    }
+
+    public override void OnGoalSetup() { }
+    public override void OnGoalFinished() { }
+    public override void OnGoalAborted() { }
+    public override bool IsGoalRelevant() { return true; }
+}
